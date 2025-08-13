a097346a5bddc5a9057d9de9cdcaeb61b79befc0
Garjanveer Sharma <garjanveersharma06@gmail.com>
2025-08-13 21:16:25 +0530

    Create 2095. Delete the Middle Node of a Linked List.cpp
    [ LeetCode ]

---
+++ 2095. Delete the Middle Node of a Linked List.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+       if(!head || !head->next) return NULL; 
+       
+        ListNode* prev = NULL ;
+        ListNode* slow = head; 
+        ListNode* fast = head; 
+
+        while(fast && fast->next){
+        prev = slow; 
+        slow = slow->next; 
+        fast = fast->next->next; 
+        }
+        prev->next = slow->next; 
+        delete slow; 
+
+        return head; 
+    }
+};
