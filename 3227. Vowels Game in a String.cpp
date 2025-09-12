fa966258a284979e63c640c576e5ff505d34ca19
Garjanveer Sharma <garjanveersharma06@gmail.com>
2025-09-12 21:18:23 +0530

    Create 3227. Vowels Game in a String.cpp
    LeetCode - DCC

---
+++ 3227. Vowels Game in a String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        string vowels = "aeiou"; 
+        for(char ch : s){
+            if(vowels.find(ch) != string::npos){
+                return true; 
+            }
+        }
+        return false; 
+    }
+};
